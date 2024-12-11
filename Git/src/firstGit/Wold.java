46547ba5f753b2d07ae0e4cc8e151faeac964acd
ArohiPawar <99589611+ArohiPawar@users.noreply.github.com>
2024-12-11 18:24:35 +0530

    m2 add by me

--- Git/src/firstGit/Wold.java
+++ Git/src/firstGit/Wold.java
@@ -3,10 +3,16 @@ package firstGit;
 public class Wold {
 public void m1()
 	{
+	System.out.println("m1 called");	
 	}
-	public static void main(String[] args) {
-		System.out.println("Hello Wold");
 
+public void m2()
+	{
+	System.out.println("m2 called");	
 	}
+public static void main(String[] args) {
+	System.out.println("Hello Wold");
+
+}
 
 }
